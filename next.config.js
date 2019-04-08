599b1d7239d369e4c97e8c01e48e672083be9236
utwo <mihai.legat@gmail.com>
2019-04-08 18:44:07 +0300

    Add deploy to now Resolve typo Add github.com to known host Add teleport-component-generators from gitlab mirror to prevent Host key verification failed on now.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withTypescript(
-  withCSS({
-    webpack(config, options) {
-      return config
-    }
-  })
-)
+module.exports = withTypescript(withCSS())
